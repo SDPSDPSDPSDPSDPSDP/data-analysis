b3f9b1328eb487363aa180ac298baed0c4e5d922
Shirin <162015519+SDPSDPSDPSDPSDPSDP@users.noreply.github.com>
2025-10-30 18:26:47 +0100

    Adding Vertical Countplots (#6)
    * added countplot_x
    * changed height
    * adjusted whitespace
    * fixed stacked barplot
    * fixed label colors
    * added datalabels to stacked plots
    * pies dynamic color labels
    * aggregated normalized plots
    * added datatypes
    * updated examples

---
+++ shirin/plot/config/types.py
@@ -0,0 +1,55 @@
+"""Type definitions for plot configuration."""
+
+from enum import Enum
+from typing import Optional, Union
+
+
+class OrderType(str, Enum):
+    """Order type for sorting plot categories.
+    
+    Options:
+        FREQUENCY: Sort by frequency (most common first)
+        ALPHABETICAL: Sort alphabetically
+    """
+    FREQUENCY = 'frequency'
+    ALPHABETICAL = 'alphabetical'
+
+
+class StackedLabelType(str, Enum):
+    """Label type for stacked plots.
+    
+    Options:
+        STANDARD: Show standard count labels
+        PERCENTAGE: Show percentage labels
+    """
+    STANDARD = 'standard'
+    PERCENTAGE = 'percentage'
+
+
+class FigureSize(str, Enum):
+    """Figure size options.
+    
+    Options:
+        DYNAMIC: Automatically calculate based on data
+        STANDARD: Use standard fixed size
+    """
+    DYNAMIC = 'dynamic'
+    STANDARD = 'standard'
+
+
+class FillMissingValues(str, Enum):
+    """Strategy for filling missing values in lineplot.
+    
+    Options:
+        SHIFT: Forward fill (use previous value)
+        ZERO: Fill with zeros
+    """
+    SHIFT = 'shift'
+    ZERO = 'zero'
+
+
+# Type aliases that accept both Enum and string
+OrderTypeInput = Union[OrderType, str]
+StackedLabelTypeInput = Optional[Union[StackedLabelType, str]]
+FigureSizeInput = Union[FigureSize, str, float]
+FillMissingValuesInput = Optional[Union[FillMissingValues, str]]
