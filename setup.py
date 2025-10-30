7fb09535a482d815286af0110ae4c52f3be07a32
Shirin <162015519+SDPSDPSDPSDPSDPSDP@users.noreply.github.com>
2025-10-30 17:03:40 +0100

    Refactor (#4)
    * fixed example notebook
    * refactor
    * updated to do
    * changed version
    * improved readability
    * pep 8
    * intellisense
    * updated typehints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shirin",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "matplotlib",
@@ -10,14 +10,7 @@ setup(
         "numpy",
         "pandas"
     ],
-<<<<<<< HEAD
     python_requires=">=3.9",
-=======
-<<<<<<< HEAD
-    python_requires=">=3.9",  # or whatever minimum version you need
-=======
->>>>>>> 98ad25ae4d7a1a08b7157f971eac37aba8804a6e
->>>>>>> 9ad1f60c7dfa9f4d533045ec406ed0c82016153d
     author="Shirin",
     description="Custom visualization tools for data science projects",
 )
