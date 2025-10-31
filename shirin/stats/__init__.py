a6e5a4b7494cea52e05d0f74d4463159dce5469d
SDPSDPSDPSDPSDPSDP <shirindepreeuw@gmail.com>
2025-10-31 17:48:26 +0100

    updated stats to be an accessor

--- shirin/stats/__init__.py
+++ shirin/stats/__init__.py
@@ -1,19 +1,28 @@
 import pandas as pd
 
+
 def print_rounded(name, value):
     print(f"{name}: {value:,.0f}".replace(',', '.'))
 
-def print_stats(df: pd.DataFrame, column: str, printing: bool=True):
 
-    stats = {
-        'mean': df[column].mean(),
-        'median': df[column].median(),
-        'min': df[column].min(),
-        'max': df[column].max(),
-    }
+@pd.api.extensions.register_series_accessor("stats")
+class StatsAccessor:
+    def __init__(self, pandas_obj):
+        self._obj = pandas_obj
     
-    if printing:
-        print_rounded("Mean", stats['mean'])
-        print_rounded("Median", stats['median'])
-        print_rounded("Minimum", stats['min'])
-        print_rounded("Maximum", stats['max'])
+    def __call__(self, printing: bool = True):
+        stats = {
+            'mean': self._obj.mean(),
+            'median': self._obj.median(),
+            'min': self._obj.min(),
+            'max': self._obj.max(),
+        }
+        
+        if printing:
+            print_rounded("Mean", stats['mean'])
+            print_rounded("Median", stats['median'])
+            print_rounded("Minimum", stats['min'])
+            print_rounded("Maximum", stats['max'])
+            return None
+        
+        return stats
