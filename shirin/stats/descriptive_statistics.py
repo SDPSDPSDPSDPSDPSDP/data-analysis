6b1f986acad307fa8bfca01153990993274af24f
SDPSDPSDPSDPSDPSDP <shirindepreeuw@gmail.com>
2025-11-02 12:16:53 +0100

    added empty count to stats

--- shirin/stats/descriptive_statistics.py
+++ shirin/stats/descriptive_statistics.py
@@ -11,11 +11,14 @@ class StatsAccessor:
         self._obj = pandas_obj
     
     def __call__(self, printing: bool = True):
+        missing_values = int(self._obj.isna().sum())
+        
         stats = {
             'mean': self._obj.mean(),
             'median': self._obj.median(),
             'min': self._obj.min(),
             'max': self._obj.max(),
+            'missing_values': missing_values,
         }
         
         if printing:
@@ -23,6 +26,8 @@ class StatsAccessor:
             print_rounded("Median", stats['median'])
             print_rounded("Minimum", stats['min'])
             print_rounded("Maximum", stats['max'])
+            if missing_values > 0:
+                print("\nMissing Values: {:,.0f}".format(missing_values).replace(',', '.'))
             return None
         
         return stats
