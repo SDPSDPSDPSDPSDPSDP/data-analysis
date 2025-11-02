55c0afb8bddf53296718f30fa730ef408c717b9c
Shirin <162015519+SDPSDPSDPSDPSDPSDP@users.noreply.github.com>
2025-11-02 12:19:58 +0100

    Update Stats Method (#11)
    * updated stats to be an accessor
    * renamed folders
    * update readme
    * added empty count to stats
    * add gitattributes

--- shirin/stats/__init__.py
+++ shirin/stats/__init__.py
@@ -1,19 +1,4 @@
-import pandas as pd
+from shirin.stats.descriptive_statistics import StatsAccessor
 
-def print_rounded(name, value):
-    print(f"{name}: {value:,.0f}".replace(',', '.'))
 
-def print_stats(df: pd.DataFrame, column: str, printing: bool=True):
-
-    stats = {
-        'mean': df[column].mean(),
-        'median': df[column].median(),
-        'min': df[column].min(),
-        'max': df[column].max(),
-    }
-    
-    if printing:
-        print_rounded("Mean", stats['mean'])
-        print_rounded("Median", stats['median'])
-        print_rounded("Minimum", stats['min'])
-        print_rounded("Maximum", stats['max'])
+__all__ = ['StatsAccessor']
