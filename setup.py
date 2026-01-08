748f28815591407ce8a7ed09fcfeda438259b166
Shirin <162015519+SDPSDPSDPSDPSDPSDP@users.noreply.github.com>
2026-01-08 16:20:12 +0100

    Update Missing INIT files (#14)
    * updated notebook
    * updated version
    * added init folders
    * updated sql db2
    * removed comments
    Co-authored-by: sdepreeu <shirin.depreeuw@minfin.fed.be>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shirin",
-    version="2.0.0",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=[],
     python_requires=">=3.9",
